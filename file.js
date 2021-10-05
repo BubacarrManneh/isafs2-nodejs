95a37a74070010a25a45c4f6e447a9484d93cc54
MacBook Air <macbookair@Air-van-MacBook.home>
2021-10-06 01:19:15 +0200

    NodeJs assignment

--- file.js
+++ file.js
@@ -1,1 +1,24 @@
 // json assignment here
+const fs = require('fs')
+
+fs.readFile('countries.json', (error, data) => {
+    if(error) {
+        return ('an error occured', error);
+    }
+    const readData = Buffer.from(data).toString();
+    console.log('data', readData)
+
+    const printJson =JSON.parse(readData)
+    const countries = printJson.map(country => {
+        return ({'Country' : country.mame})
+    })
+
+    const msg = 'The list of country names'
+    const countriesData = JSON.stringify(countries)
+
+    fs.writeFile('countries.json', msg, countriesData, error => {
+        if(error) {
+            return ('an error occured', error);
+        }
+    })
+})
--- server.js
+++ server.js
@@ -1,1 +1,27 @@
 // http server here
+const http = require('http'),
+
+const server = http.createServer((req, res) => {
+    const method = req.method
+
+    switch(method){
+        case 'GET': res.write('GET response from countries name')
+            res.end()
+            break
+        case 'POST': res.write('POST countries name')
+            res.end()
+            break
+        case 'DELETE': res.write('DELETE countries name')
+            res.end()
+            break
+        default: res.write('No response')
+    }
+
+})
+
+server.listen(3000, error => {
+    if(!error === ok){
+        console.log('error while listening', error)
+    }
+    console.log('server is listening on port 3000')
+})
